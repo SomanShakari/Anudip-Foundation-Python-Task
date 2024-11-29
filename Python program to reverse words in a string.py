c23524026dce93cde45d0c91acf2f879cf593cda
Soman Shakari <168862564+SomanShakari@users.noreply.github.com>
2024-11-29 20:52:58 +0530

    Add files via upload

---
+++ Python program to reverse words in a string.py
@@ -0,0 +1,14 @@
+# 3. Write a Python program to reverse words in a string String = “Deeptech Python Training”
+
+# function
+def reversed_string(string):
+    result = string[::-1]
+    return result
+
+String = "Deeptech Python Training"
+
+result = reversed_string(String)
+print(f"The reversed of the fiven string is {result}")
+
+
+
